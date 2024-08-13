5a28c397db790a6abc425f6adafbb79eeefd61d9
Deepak8926 <dipaksahu557@gmail.com>
2024-08-13 10:49:33 +0530

    Add files via upload

---
+++ Password generator.py
@@ -0,0 +1,14 @@
+import random
+
+def generate_password(length):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+    password = ""
+
+    for i in range(length):
+        password += random.choice(characters)
+
+    return password
+
+length = int(input("Enter the desired length of password:"))
+password = generate_password(length)
+print("Generated password:", password)     
